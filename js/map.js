ea085e00ec3ead4dce939c558185ab26c6221f32
jihen403 <jihen403@gmail.com>
2024-05-20 23:32:52 +0900

    gdi

--- js/map.js
+++ js/map.js
@@ -15,6 +15,37 @@ var map = new kakao.maps.Map(mapContainer, mapOption);
 var ps = new kakao.maps.services.Places(); 
 
 // 사용자의 현재 위치 얻기
+function currentLocation() {
+	// HTML5의 geolocation으로 사용할 수 있는지 확인합니다
+	if (navigator.geolocation) {
+
+		// GeoLocation을 이용해서 접속 위치를 얻어옵니다
+		navigator.geolocation.getCurrentPosition(function(position) {
+
+			var lat = position.coords.latitude, // 위도
+			    lon = position.coords.longitude; // 경도
+
+			var locPosition = new kakao.maps.LatLng(lat, lon); // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
+			var message = '<div style="padding:5px;">현위치</div>'; // 인포윈도우에 표시될 내용입니다
+
+			// 마커와 인포윈도우를 표시합니다
+			displayMarker(locPosition, message);
+		});
+	} else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
+
+		var locPosition = new kakao.maps.LatLng(37.4812845080678, 126.952713197762),
+			message = '현재 위치를 알 수 없어 기본 위치로 이동합니다.'
+
+		currentLatLon['lat'] = 33.450701
+		currentLatLon['lon'] = 126.570667
+
+		displayMarker(locPosition, message);
+	}
+	return true
+}
+
+currentLocation();
+
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
         const lat = position.coords.latitude;
@@ -28,13 +59,14 @@ if (navigator.geolocation) {
        location: new kakao.maps.LatLng(lat, lng),
        radius: 500
         };
-        ps.keywordSearch('일식집', placesSearchCB, options);
+        ps.categorySearch('BK9', placesSearchCB, {useMapBounds:true}); 
     }, () => {
         console.error('사용자 위치를 가져올 수 없습니다.');
     });
         } else {
     console.error('Geolocation API를 지원하지 않습니다.');
 }
+
 
 // 키워드 검색 완료 시 호출되는 콜백함수 입니다
 function placesSearchCB (data, status, pagination) {
@@ -56,4 +88,4 @@ function displayMarker(place) {
     // 지도 중심을 무작위로 선택된 장소로 이동
     map.setCenter(new kakao.maps.LatLng(place.y, place.x));
 }
-};
+}
