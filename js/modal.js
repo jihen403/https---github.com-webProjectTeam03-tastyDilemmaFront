9ae581227c1569c4b9be8f042da45ea8fe4244a2
kosanghyu <rhgustkd6568@naver.com>
2024-05-20 11:48:10 +0900

    update main, modal html , modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,7 @@
 // DOMContentLoaded 이벤트를 사용하여 DOM이 완전히 로드되었을 때 실행될 함수를 등록
 document.addEventListener('DOMContentLoaded', () => {
+  // 오버레이 요소를 가져옴
+  const overlay = document.getElementById('overlay');
   // 모달 요소를 가져옴
   const modal = document.getElementById('myModal');
   // 모달 닫기 버튼 (x 버튼) 요소를 가져옴
@@ -8,26 +10,18 @@ document.addEventListener('DOMContentLoaded', () => {
   const cancelButton = document.getElementById('cancelButton');
   // 선택 버튼 요소를 가져옴
   const selectButton = document.getElementById('selectButton');
-
-  // 모달 닫기 함수 정의
-  function closeModal() {
-    modal.style.display = "none"; // 모달을 숨김
-  }
-
-  // 닫기 버튼 클릭 시 모달 닫기
-  closeBtn.addEventListener('click', closeModal);
-
-  // 취소 버튼 클릭 시 모달 닫기
-  cancelButton.addEventListener('click', closeModal);
-
-  // 선택 버튼 클릭 시 알림 및 모달 닫기
-  selectButton.addEventListener('click', () => {
-    alert('선택 되었습니다.');
-    closeModal();
-  });
 });
 
 // 모달 열기
 function openModal() {
   document.getElementById("myModal").style.display = "block";
 }
+
+// 모달 닫기
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+
+  
+  
